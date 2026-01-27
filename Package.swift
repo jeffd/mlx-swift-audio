bb90143d86a0dd702e2ff0a3ebe6297ac80faaca
Jeff Dlouhy <github@jeffd.org>
2026-01-27 18:40:25 +0100

    Add explicit MLX module dependencies

--- Package.swift
+++ Package.swift
@@ -31,6 +31,11 @@ let package = Package(
       dependencies: [
         .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
         .product(name: "MLXLLM", package: "mlx-swift-lm"),
+        // Explicit dependencies required for explicit module builds
+        .product(name: "MLX", package: "mlx-swift"),
+        .product(name: "MLXNN", package: "mlx-swift"),
+        .product(name: "MLXFast", package: "mlx-swift"),
+        .product(name: "MLXRandom", package: "mlx-swift"),
         .product(name: "MLXFFT", package: "mlx-swift"),
         .product(name: "Transformers", package: "swift-transformers"),
         .product(name: "SwiftTiktoken", package: "swift-tiktoken"),
