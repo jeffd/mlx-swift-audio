397b34ef92a96cfb87bcfe161f2c665e100eb6e2
Jeff Dlouhy <github@jeffd.org>
2026-03-06 09:25:54 +0100

    Remove Kokoro

--- Package.swift
+++ Package.swift
@@ -5,15 +5,9 @@ let package = Package(
   name: "mlx-audio",
   platforms: [.macOS("15.4"), .iOS("18.4")],
   products: [
-    // Core library without Kokoro (no GPLv3 dependencies)
     .library(
       name: "MLXAudio",
       targets: ["MLXAudio"],
-    ),
-    // Separate Kokoro package (depends on GPLv3-licensed espeak-ng)
-    .library(
-      name: "Kokoro",
-      targets: ["Kokoro"],
     ),
   ],
   dependencies: [
@@ -21,9 +15,6 @@ let package = Package(
     .package(url: "https://github.com/ml-explore/mlx-swift", branch: "main"),
     .package(url: "https://github.com/huggingface/swift-transformers", .upToNextMinor(from: "1.1.0")),
     .package(url: "https://github.com/DePasqualeOrg/swift-tiktoken", branch: "main"),
-    // espeak-ng is GPLv3 licensed - only linked when using Kokoro
-    // TODO: Switch back to upstream after https://github.com/espeak-ng/espeak-ng/pull/2327 is merged
-    .package(url: "https://github.com/DePasqualeOrg/espeak-ng-spm.git", branch: "fix-path-espeak-data-macro"),
   ],
   targets: [
     .target(
@@ -45,15 +36,6 @@ let package = Package(
       resources: [
         .process("TTS/OuteTTS/default_speaker.json"), // Default speaker profile for OuteTTS
       ],
-    ),
-    .target(
-      name: "Kokoro",
-      dependencies: [
-        "MLXAudio",
-        .product(name: "libespeak-ng", package: "espeak-ng-spm"),
-        .product(name: "espeak-ng-data", package: "espeak-ng-spm"),
-      ],
-      path: "package/TTS/Kokoro",
     ),
     .testTarget(
       name: "MLXAudioTests",
